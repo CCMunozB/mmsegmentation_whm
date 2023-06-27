e24bab7079832d8edac265035115afd446f695c9
CCMunozB <cristian.amb.30@gmail.com>
2023-06-27 17:34:27 -0400

    Adpat for 1 class

--- mmseg/datasets/wmh.py
+++ mmseg/datasets/wmh.py
@@ -10,19 +10,19 @@ class WMHDataset(BaseSegDataset):
     ``reduce_zero_label`` should be set to True. The ``img_suffix`` and
     ``seg_map_suffix`` are both fixed to '.tif'.
     """
-    METAINFO = dict(
-        classes=('background', 'WHM'),
-        palette=[[0, 0, 0], [233, 155, 155]])
+    #METAINFO = dict(
+    #    classes=('background', 'WHM'),
+    #    palette=[[0, 0, 0], [233, 155, 155]])
 
     def __init__(self,
                  img_suffix='.tiff',
                  seg_map_suffix='.png',
-                 reduce_zero_label=False,
-                 ignore_index=0,
+                 reduce_zero_label=True,
+                 #ignore_index=0,
                  **kwargs) -> None:
         super().__init__(
             img_suffix=img_suffix,
             seg_map_suffix=seg_map_suffix,
             reduce_zero_label=reduce_zero_label,
-            ignore_index=ignore_index,
+            #ignore_index=ignore_index,
             **kwargs)
