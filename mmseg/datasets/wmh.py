1e54de5a5d704a5cbae2b74ba18c7e9f50db539a
CCMunozB <cristian.amb.30@gmail.com>
2023-06-27 14:52:01 -0400

    whm dataset update v0.1

--- mmseg/datasets/wmh.py
+++ mmseg/datasets/wmh.py
@@ -12,15 +12,17 @@ class WMHDataset(BaseSegDataset):
     """
     METAINFO = dict(
         classes=('background', 'WHM'),
-        palette=[[255, 255, 255], [255, 0, 0]])
+        palette=[[255, 255, 255], [233, 0, 0]])
 
     def __init__(self,
                  img_suffix='.tiff',
                  seg_map_suffix='.png',
                  reduce_zero_label=True,
+                 ignore_index: int = 255,
                  **kwargs) -> None:
         super().__init__(
             img_suffix=img_suffix,
             seg_map_suffix=seg_map_suffix,
             reduce_zero_label=reduce_zero_label,
+            ignore_index=ignore_index,
             **kwargs)
