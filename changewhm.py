0a5932954cc0e648b674880283af91c299e1e39a
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 13:30:53 -0400

    add change to dataset labels

---
+++ changewhm.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import os
+
+train_label_path = "./data/train/label/"
+val_label_path = "./data/val/label/"
+
+for n,val in enumerate(os.listdir(val_label_path)):
+    data = cv2.imread(val_label_path + val)
+    data[data == 255] = 233
+    cv2.imwrite(val_label_path + val, data)
+    print(n, end="\r")
+
+for n,val in enumerate(os.listdir(train_label_path)):
+    data = cv2.imread(train_label_path + val)
+    data[data == 255] = 233
+    cv2.imwrite(train_label_path + val, data)
+    print(n, end="\r")
