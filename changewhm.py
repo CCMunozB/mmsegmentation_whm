304bebac032f8f17d269b6cb2fddbdc3f617e665
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 14:26:55 -0400

    changewhm update

--- changewhm.py
+++ changewhm.py
@@ -7,12 +7,12 @@ val_label_path = "./data/val/label/"
 
 for n,val in enumerate(os.listdir(val_label_path)):
     data = tifffile.imread(val_label_path + val)
-    data[data == 1] = 254
+    data[data == 244] = 1
     tifffile.imwrite(val_label_path + val, data)
     print(n, end="\r")
 
 for n,val in enumerate(os.listdir(train_label_path)):
     data = tifffile.imread(train_label_path + val)
-    data[data == 1] = 254
+    data[data == 243] = 1
     tifffile.imwrite(train_label_path + val, data)
     print(n, end="\r")
