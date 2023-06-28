5edd584272346bf7c672c6afab4f7f904d7d5eac
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 17:05:14 -0400

    change pallete

--- mmseg/datasets/wmh.py
+++ mmseg/datasets/wmh.py
@@ -12,7 +12,7 @@ class WMHDataset(BaseSegDataset):
     """
     METAINFO = dict(
        classes=('WHM'),
-       palette=[233])
+       palette=[[233, 155, 155]])
 
     def __init__(self,
                  img_suffix='.tiff',
