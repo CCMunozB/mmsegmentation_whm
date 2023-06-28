a4df5d5e594bb38af4773654192a819b09fda25d
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 14:22:47 -0400

    changewhm update

--- changewhm.py
+++ changewhm.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import tifffile
 import os
@@ -7,13 +6,13 @@ train_label_path = "./data/train/label/"
 val_label_path = "./data/val/label/"
 
 for n,val in enumerate(os.listdir(val_label_path)):
-    data = cv2.imread(val_label_path + val)
-    data[data == 233] = 1
-    tifffile.imwrite(val_label_path + val, data[:,:,0])
+    data = tifffile.imread(val_label_path + val)
+    data[data == 1] = 254
+    tifffile.imwrite(val_label_path + val, data)
     print(n, end="\r")
 
 for n,val in enumerate(os.listdir(train_label_path)):
-    data = cv2.imread(train_label_path + val)
-    data[data == 233] = 1
-    tifffile.imwrite(train_label_path + val, data[:,:,0])
+    data = tifffile.imread(train_label_path + val)
+    data[data == 1] = 254
+    tifffile.imwrite(train_label_path + val, data)
     print(n, end="\r")
