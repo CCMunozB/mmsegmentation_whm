6f77f744a2a137f39e1b75d3609f4c1daa153f22
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 14:07:33 -0400

    Change data label value

--- changewhm.py
+++ changewhm.py
@@ -8,12 +8,12 @@ val_label_path = "./data/val/label/"
 
 for n,val in enumerate(os.listdir(val_label_path)):
     data = cv2.imread(val_label_path + val)
-    #data[data == 255] = 233
+    data[data == 233] = 1
     tifffile.imwrite(val_label_path + val, data[:,:,0])
     print(n, end="\r")
 
 for n,val in enumerate(os.listdir(train_label_path)):
     data = cv2.imread(train_label_path + val)
-    #data[data == 255] = 233
+    data[data == 233] = 1
     tifffile.imwrite(train_label_path + val, data[:,:,0])
     print(n, end="\r")
