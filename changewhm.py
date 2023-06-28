49fc6742a0963dba80ac6b4d15a5119999b3f59b
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 13:41:03 -0400

    Change dataset labels

--- changewhm.py
+++ changewhm.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import tifffile
 import os
 
 train_label_path = "./data/train/label/"
@@ -7,12 +8,12 @@ val_label_path = "./data/val/label/"
 
 for n,val in enumerate(os.listdir(val_label_path)):
     data = cv2.imread(val_label_path + val)
-    data[data == 255] = 233
-    cv2.imwrite(val_label_path + val, data)
+    #data[data == 255] = 233
+    tifffile.imwrite(val_label_path + val, data[:,:,0])
     print(n, end="\r")
 
 for n,val in enumerate(os.listdir(train_label_path)):
     data = cv2.imread(train_label_path + val)
-    data[data == 255] = 233
-    cv2.imwrite(train_label_path + val, data)
+    #data[data == 255] = 233
+    tifffile.imwrite(train_label_path + val, data[:,:,0])
     print(n, end="\r")
