f3af06c573864bcacc5bfa3a29716a139e28facc
CCMunozB <cristian.amb.30@gmail.com>
2023-06-28 16:53:43 -0400

    Update ignore index

--- configs/whm/swin_upernet_whm.py
+++ configs/whm/swin_upernet_whm.py
@@ -20,12 +20,12 @@ model = dict(
                      num_classes=2,
                      out_channels=1,
                      loss_decode=dict(
-                         type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
+                         type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0, avg_non_ignore=True)),
     auxiliary_head=dict(in_channels=384, 
                         num_classes=2,
                         out_channels=1,
                         loss_decode=dict(
-                         type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.4)))
+                         type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.4, avg_non_ignore=True)))
 
 # AdamW optimizer, no weight decay for position embedding & layer norm
 # in backbone
