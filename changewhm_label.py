7853ef37560bbfcc1a7dadc47da9ea9bb61a72cf
CCMunozB <cristian.amb.30@gmail.com>
2023-06-29 15:45:19 -0400

    Update V1.8

--- changewhm_label.py
+++ changewhm_label.py
@@ -1,17 +1,14 @@
 import numpy as np
-import tifffile
+import cv2
 import os
 
 
 def change(dir):
     lista = np.sort(os.listdir(dir))
     for val in lista:
-        data = tifffile.imread(dir+ val)
+        data = cv2.imread(dir+ val)
         im = np.invert(data)
-        data = np.expand_dims(data, 2)
-        im = np.expand_dims(im, 2)
-        final = np.concatenate((data,im), axis=2)
-        tifffile.imwrite(dir + val, final)
+        cv2.imwrite(dir + val, im)
         print(val, end="\r")
 
 if __name__=='__main__':
