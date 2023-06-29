f00f45ed1ad2861be48569f35f38433adbbd8a1c
CCMunozB <cristian.amb.30@gmail.com>
2023-06-29 15:22:02 -0400

    Update V1.5

--- changewhm_label.py
+++ changewhm_label.py
@@ -8,7 +8,10 @@ def change(dir):
     for val in lista:
         data = tifffile.imread(dir+ val)
         im = np.invert(data)
-        tifffile.imwrite(dir + val, im)
+        data = np.expand_dims(data, 2)
+        im = np.expand_dims(im, 2)
+        final = np.concatenate((data,im), axis=2)
+        tifffile.imwrite(dir + val, final)
         print(val, end="\r")
 
 if __name__=='__main__':
